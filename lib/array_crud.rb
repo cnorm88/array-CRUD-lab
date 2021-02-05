c644da08a19fb1a25f4ceffa16e82190c9d31b83
cnorm88 <cnorm88@gmail.com>
2021-02-05 15:45:47 -0500

    commit tree ebc3119d5bafc02ca076b316e232d1c3592c76a8

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,45 @@
 def create_an_empty_array
-  
+    []
 end
 
 def create_an_array
-  
+  cats = ["speedy", "sesame", "spinach", "sally"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array = ["speedy", "sesame", "spinach", "sally"]
+    element = "arrays!"
+    array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array = ["speedy", "sesame", "spinach", "sally"]
+    element = "wow"
+    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array = ["speedy", "sesame", "spinach", "arrays!"]
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array = ["wow","speedy", "sesame", "spinach", "arrays!"]
+    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"] 
+    index_number = 2
+    array[index_number]  
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array.last
 end
